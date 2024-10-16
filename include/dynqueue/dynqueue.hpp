5d46d42eb3cd3472a1109236e32a013b331b1352
Sebastian Hauer <hauer@psicode.com>
2024-10-15 21:07:46 -0400

    :sparkles: [dynqueue] Made the queue generic
    Problem:
    - The `value_type` is hardcoded to `int`.
    - The memory management is making use of raw pointers.
    
    Solution:
    - Turn the queue into a template taking the value type.
    - Utilize `std::unique_ptr` to manage the memory.

--- include/dynqueue/dynqueue.hpp
+++ include/dynqueue/dynqueue.hpp
@@ -1,50 +1,41 @@
 #include <algorithm>
 #include <cstddef>
+#include <memory>
 
-struct dynqueue
-{
-    explicit dynqueue(const std::size_t capacity = 32) : data_{new int[capacity]}, data_end_{data_ + capacity}, head_{data_}, tail_{data_}
-    {
-    }
+template <typename T> struct dynqueue {
+    using value_type = T;
 
-    std::size_t capacity() const
-    {
-        return data_end_ - data_;
-    }
+    explicit dynqueue(const std::size_t capacity = 32)
+        : data_{std::make_unique<value_type[]>(capacity)},
+          data_end_{data_.get() + capacity}, head_{data_.get()},
+          tail_{data_.get()} {}
 
-    std::size_t size() const
-    {
-        return tail_ - head_;
-    }
+    std::size_t capacity() const { return data_end_ - data_.get(); }
 
-    void enqueue(int key)
-    {
-        if (tail_ >= data_end_)
-        {
+    std::size_t size() const { return tail_ - head_; }
+
+    void enqueue(value_type key) {
+        if (tail_ >= data_end_) {
             grow();
         }
         *tail_ = key;
         ++tail_;
     }
 
-private:
-    void grow()
-    {
+  private:
+    void grow() {
         const auto sz = size();
         const auto new_capacity = capacity() * 2;
-        auto new_data = new int[new_capacity];
-        auto new_data_end = data_ + new_capacity;
-        std::copy(head_, data_end_, new_data);
-        head_ = new_data;
+        auto new_data = std::make_unique<value_type[]>(new_capacity);
+        std::copy(head_, data_end_, new_data.get());
+        head_ = new_data.get();
         tail_ = head_ + sz;
-
-        delete[] data_;
-        data_ = new_data;
-        data_end_ = data_ + new_capacity;
+        data_.swap(new_data);
+        data_end_ = data_.get() + new_capacity;
     }
 
-    int *data_ = nullptr;
-    int *data_end_ = nullptr;
-    int *head_ = nullptr;
-    int *tail_ = nullptr;
+    std::unique_ptr<value_type[]> data_;
+    value_type* data_end_;
+    value_type* head_;
+    value_type* tail_;
 };
--- tests/test_dynqueue.cpp
+++ tests/test_dynqueue.cpp
@@ -2,16 +2,14 @@
 
 #include <dynqueue/dynqueue.hpp>
 
-TEST_CASE("new queues are empty and have the expected capacity")
-{
-    auto q = dynqueue(16);
+TEST_CASE("new queues are empty and have the expected capacity") {
+    auto q = dynqueue<int>(16);
     REQUIRE(q.size() == 0);
     REQUIRE(q.capacity() == 16);
 }
 
-TEST_CASE("queue size increases while enqueing")
-{
-    auto q = dynqueue(2);
+TEST_CASE("queue size increases while enqueing") {
+    auto q = dynqueue<int>(2);
     q.enqueue(1);
     REQUIRE(q.size() == 1);
     q.enqueue(2);
@@ -20,9 +18,8 @@ TEST_CASE("queue size increases while enqueing")
     REQUIRE(q.size() == 3);
 }
 
-TEST_CASE("queue capacity should double")
-{
-    auto q = dynqueue(1);
+TEST_CASE("queue capacity should double") {
+    auto q = dynqueue<int>(1);
     REQUIRE(q.capacity() == 1);
     q.enqueue(1);
     REQUIRE(q.capacity() == 1);
